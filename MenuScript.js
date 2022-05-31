1f008470589ae38d051b104a47c31dbb9bfd0751
DarklordDoD <56915597+DarklordDoD@users.noreply.github.com>
2022-05-31 12:37:59 +0200

    Framework
    tilføjet sprog skifte knap.

--- MenuScript.js
+++ MenuScript.js
@@ -1,8 +1,27 @@
 // JavaScript Document
 
-const togglebutton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+//burger menu
+const togglebutton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 togglebutton.addEventListener('click',() => {
 	navbarLinks.classList.toggle('active')
 })
+
+//skift sprog knap
+const alleSprog = ["DK","EN"];
+let sprogLength = alleSprog.length;
+let valgtSprog = 1;
+const titelSprog = ["Mærke Navn","Brand Name",]
+
+function sprog(){	
+	//roter mellem sprogene
+	if (valgtSprog == sprogLength){
+		valgtSprog = 1;
+	} else {
+		valgtSprog += 1;
+	}
+	//ændre teksten
+	document.getElementById("sprogText").innerHTML = alleSprog[valgtSprog-1];
+	document.getElementById("brandTitle").innerHTML = titelSprog[valgtSprog-1];
+}
