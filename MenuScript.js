71f9fa41035d01e660fbc585c296117a6ed18dcc
DarklordDoD <56915597+DarklordDoD@users.noreply.github.com>
2022-06-10 12:46:50 +0200

    info kan oversættes nu

--- MenuScript.js
+++ MenuScript.js
@@ -79,7 +79,33 @@ const omP2 = ["Vi drømmer om, at der i fremtiden ikke vil være brug for vores 
 
 const omP3 = ["Vi ved godt at vi alene ikke kan redde hele verdenen, men vi vil gøre så meget vi kan. Derfor er vores prioritering at gøre så processerne er så bæredygtige så muligt.","We are aware that we alone cannot save the world, but we will do what we can. That is why our priority is to make the individual processes as sustainable as possible."];
 
-/*----------------------------(roter mellem sprogene)--------------------------------*/
+/*----------------------------(Info side)--------------------------------*/
+
+const infoP1 = ["Det materiale som vi udvinder og genbruger kan komme mange steder fra. Alt fra gamle dåser til store fabriksmaskiner, som over tid er blevet for slidte. Man kan endda finde affald begravet under jorden, som førhen kunne have været blevet brugt til noget, som en skrue fra en traktor, som den tabte midt i høsten","The materials that we extract and recycle comes from many places. Everything from old aluminum cans to big factory machinery, which over time has stopped being functional. We even find waste buried underground, which has been used in the past, like a screw from a tractor, that it dropped during a harvest."];
+
+const infoP2 = ["Affaldet bliver sorteret før genanvendelse. Det gør man ved hjælp af roterende magneter, som er for enden af nogle ¨samlebånd¨. Der er kun nogle metaller som har magnetiske egenskaber. Disse er primært jern, nikkel og stål.","The materials that we extract and recycle comes from many places. Everything from old aluminum cans to big factory machinery, which over time has stopped being functional. We even find waste buried underground, which has been used in the past, like a screw from a tractor, that it dropped during a harvest."];
+
+const infoP3 = ["Derefter kommer affaldet igennem et shredder-anlæg. Som navnet antyder, er det en maskine som hakker affaldet i mindre stykker. Efter en sidste sortering hvor affaldet bliver opdelt efter materiale, bliver affaldet smeltet om, og derved har man nu rent råt materiale, som kan få nyt liv gennem genanvendelse.","After sortation, the waste is put through a shredder plant. As the name suggests, this is where the waste is cut down into smaller pieces. After that, the waste is sorted depending on what material they are made of. Now the materials from the waste can be melted down, and be given new life through recycling."];
+
+const infoTittel2 = ["Tips til genbrug / bæredygtighed I hjemmet:","Tips for recycling / sustainability in the home:"]
+
+const info2P1 = ["Vi gør os umage med at få genbrugt så meget affald som muligt, men hvis vores planet skal blive renere, må vi alle gøre en indsats. Derfor har vi samlet nogle tips til jer derhjemme, til hvordan jeres hverdag kan blive mere miljøvenlig.","We make a great effort to recycle as much waste as we can, but if we are to make our planet cleaner, we must all do our part. That is why we have collected some tips for you at home, to make your everyday life more environmentally friendly."];
+
+const info2P2 = ["Det mest bæredygtige forbrug er det forbrug, som slet ikke finder sted. Når du er ude og handle efter dagligdagsvarer er det derfor en god idé at overveje hvorvidt man har brug for det man putter i kurven. Begræns dig selv til kun at handle ind til det, som I i jeres husstand kan nå at forbruge, før det bliver dårligt.","The most sustainable consumption is the consumption that does not take place at all. When you are out shopping for groceries, it is therefore a good idea to consider what you put in your basket. Limit yourself to shopping only what you in your household can consume before the food goes bad."];
+
+const info2P3 = ["Når du er ude og handle er det også en god idé at købe økologiske fødevarer. På den måde slipper du som udgangspunkt for, at der er rester sprøjtemidler i din mad. Det er ikke kun frugt og grønt som blive produceret uden sprøjtemidler. Det samme gælder de planter, som bruges til foder for økologisk opdrættede dyr. Det er kun ganske få tilsætningsstoffer som er tilladte i økologiske fødevarer. Det betyder at du kan være med til mindske belastningen af naturen, fremme bedre dyrevelfærd og reducere dit og din families indtag af sprøjtemiddelrester.","When you are out shopping it is also a good idea to buy organic foods. That way you essentially avoid any pesticide residues in your food. It is not only fruits and vegetable that are produced without pesticides. The same goes for plants used for fodder for organically reared animals. There are only a few additives that are allowed in organic foods. This means that you can help reduce the burden on nature, promote better animal welfare and reduce yours and your family’s intake of pesticide residues."];
+
+const info2P4 = ["Dertil er det også en god idé at vælge miljømærkede forbrugsvarer. Uanset hvilket produkt du vælger, kan du være sikker på, at hvis det er svanemærket eller har EU-blomsten på sig, så er produktet blandt de miljømæssigt bedste i deres kategori. Det kan du fordi de stiller høje krav til produkterne, og derfor bliver produkterne nøje fulgt, for at være sikker på, at processen skåner miljøet så meget som muligt.","In addition, it is also a good idea to choose eco-labeled consumer goods. Whichever product you choose, you can be sure that if it is Nordic Eco-labeled or has the EU-flower on it, the product is among the environmentally best in their category. This is because they place high demands for the products, and therefore they monitor the product through the whole process, to ensure that the environment isn’t hurt."];
+
+const info2P5 = ["Når du er ude og handle vil vi også anbefale at kigge efter Fairtrade-mærket. Fairtrade er en international mærkningsordning, som arbejder for at bønder og arbejdere i nogle af verdens fattigste lande kan få en mere fair indtægt, og bedre arbejdsvilkår.","When you are out shopping, we also recommend looking for the Fairtrade mark. Fairtrade is an international organization, that works to ensure that farmers and workers in some of the world’s poorest countries can have a fairer income and better working conditions."];
+
+const info2P6 = ["Du kan også skåne miljøet og din pengepung, ved at købe ting brugt. Det kunne f.eks. være møbler, cykler, bøger og tøj. Gå til din nærmeste genbrugsbutik og kig dig omkring. Alternativt kan du også selv donere det du ikke længere kan bruge, som tøj du eller dine børn er vokset ud af, eller gammelt legetøj som ellers bare ligger og samler støv. Du kan også lave opslag på din lokale Facebook-side, hvor du kan sætte dine ting op til fri afhentning, hvis du gerne vil undgå at tage turen ned til en genbrugsforretning eller en station.","You can also save the environment - and a lot of money – by buying things used. It could, for example, be furniture, bicycles, books or clothes. Go to your nearest thrift store and have a look around. Alternatively, you can also donate what you no longer use, such as clothes that you or your children have outgrown, or old toys that otherwise just lie and collect dust. You can also make a post on your local Facebook-page, where you put your things up for free pickup, if you would like to avoid a trip down to the thrift store or station."];
+
+const info2P7 = ["Du kan beholde krukker og flasker af glas, og bruge dem igen til at holde på noget andet. De kan blandt andet bruges til syltetøj/marmelade, hyldeblomstsaft eller stearinlys. Man kan også bruge dem som potter til planter, skulle man mangle nogle af dem.","You can keep jars and bottles of glass and use them again to hold something else. They can be used to store jam/marmalade, elderflower juice candles, among other things. They can also be used as pots for plants, should you need some."];
+
+const info2P8 = ["Udover alle disse tips vil vi anbefale at følge den lokale affaldssortering som er opsat af din kommune. Sørg for at det skrald du har som du ikke kan genbruge kommer i de rigtige affaldscontainere, så det nemmere kan blive bearbejdet ude på affaldscentrerne.","In addition to all these tips, we would recommend following the local waste sorting set up by your municipality. Make sure that waste that you cannot recycle gets into the right waste containers, so that it can be more easily processed at the waste centers."];
+
+/*----------------------------(Roter mellem sprogene)--------------------------------*/
 function sprog(){
 	
 	
@@ -93,10 +119,10 @@ function sprog(){
 }
 
 function sprogSkift(){
-	console.log("Sprog Loadet")
 	
 	var abenVindu = window.location.pathname;
 	var denneSide = abenVindu.split("/").pop();
+	console.log(denneSide);
 	
 	/*----------------------------(Menu)--------------------------------*/
 	document.getElementById("sprogText").innerHTML = alleSprog[valgtSprog-1];
@@ -133,7 +159,21 @@ function sprogSkift(){
 	}
 	
 	/*----------------------------(Info side)--------------------------------*/
-	
+	if (denneSide == "info.html"){
+		document.getElementById("infoP1").innerHTML = infoP1[valgtSprog-1];
+		document.getElementById("infoP2").innerHTML = infoP2[valgtSprog-1];
+		document.getElementById("infoP3").innerHTML = infoP3[valgtSprog-1];
+		document.getElementById("infoP3").innerHTML = infoP3[valgtSprog-1];
+		document.getElementById("infoTittel2").innerHTML = infoTittel2[valgtSprog-1];
+		document.getElementById("info2P1").innerHTML = info2P1[valgtSprog-1];
+		document.getElementById("info2P2").innerHTML = info2P2[valgtSprog-1];
+		document.getElementById("info2P3").innerHTML = info2P3[valgtSprog-1];
+		document.getElementById("info2P4").innerHTML = info2P4[valgtSprog-1];
+		document.getElementById("info2P5").innerHTML = info2P5[valgtSprog-1];
+		document.getElementById("info2P6").innerHTML = info2P6[valgtSprog-1];
+		document.getElementById("info2P7").innerHTML = info2P7[valgtSprog-1];
+		document.getElementById("info2P8").innerHTML = info2P8[valgtSprog-1];
+	}
 }
 
 
